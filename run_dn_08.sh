66b99b2eb324ee055f8ea2a9191078cdf52c6671
Jeff-eve <55553879+Jeff-eve@users.noreply.github.com>
2020-12-03 01:53:34 +0000

    Add files via upload

---
+++ run_dn_08.sh
@@ -0,0 +1,1 @@
+python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --num_stages 2 --num_blocks_per_stage 3 --experiment_name DN_08_experiment --use_gpu True --num_classes 100 --block_type 'dense_block' --continue_from_epoch -1
